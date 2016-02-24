71793d8a38d42baa614a9204eb5142ac9df1c82c
group <galin_psy@abv.bg>
2016-02-24 21:09:09 +0200

    Masive FOR

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//ѕрекопирайте един масив от цели числа в друг масив. 
+public class Main {
+	public static void main(String[] args) {
+		
+		int[] numbers = new int [] {1, 2, 3, 4};
+		int[] numbers2 = new int [numbers.length];
+		
+		for (int i = 0; i < numbers.length; i++) {
+			System.out.println("position[" + i + "] = "+numbers[i]);
+
+	}
+
+}
+}
