f0878311a5a7e33f3353d0a31f3210592e3f29b4
group <galin_psy@abv.bg>
2016-02-24 21:48:09 +0200

    Array + for copy

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 //ѕрекопирайте един масив от цели числа в друг масив. 
 public class Main {
 	public static void main(String[] args) {
-		
-		int[] numbers = new int [] {1, 2, 3, 4};
-		int[] numbers2 = new int [numbers.length];
-		
-		for (int i = 0; i < numbers.length; i++) {
-			System.out.println("position[" + i + "] = "+numbers[i]);
 
+		int[] numbers1 = new int[] { 1, 2, 3, 4 };
+		int[] numbers2 = new int[numbers1.length];
+
+		/*
+		 * Copy array.
+		 */
+
+		for (int i = 0; i < numbers1.length; i++) {
+			numbers2[i] = numbers1[i];
+		}
+		for (int i = 0; i < numbers1.length; i++) {
+			System.out.println(numbers1[i] + " " + numbers2[i]);
+		}
 	}
-
-}
 }
